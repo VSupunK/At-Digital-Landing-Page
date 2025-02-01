70f644c77b600a4f40d644a2cc47691ebe15558e
Supun Kalhara <116533030+VSupunK@users.noreply.github.com>
2025-02-01 06:38:56 +0530

    Add Button and FAQItem components to the application

---
+++ src/components/FAQItem.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const FAQItem = ({ question, answer }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return <div className="border border-dashed"></div>;
+};
+
+export default FAQItem;
